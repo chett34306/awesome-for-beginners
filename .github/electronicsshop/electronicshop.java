a38f08fd5b805bea5744c10ab8aa080e6fabab80
Praveen Chettypally <thankyou4email@gmail.com>
2022-11-24 10:59:03 -0800

    solved couple more problems from hackerrank site

--- .github/electronicsshop/electronicshop.java
+++ .github/electronicsshop/electronicshop.java
@@ -2,6 +2,7 @@
 
 package electronicsshop;
 
+import java.util.Collections;
 import java.util.List;
 
 public class electronicshop {
@@ -25,4 +26,33 @@ public class electronicshop {
         System.out.println("max budget cost" + largest_closetobudget);
         
     }
+
+
+    public static int electronicsshop1(int[] drives, int[] keyboards, int b)
+    {
+        int max_value = -1;
+         int max_keyboard_value = keyboards[0];
+         int max_drives_value = drives[0];
+         List<Integer> values = new ArrayList<Integer>();
+         for(int i = 0; i < keyboards.length; i++)
+         {
+             for(int j = 0; j < drives.length; j++)
+             {
+                 if (keyboards[i] + drives[j] <= b)
+                 {
+                    values.add(keyboards[i] + drives[j]);
+                 }
+             }
+         }
+         System.out.println(values);
+         Collections.sort(values, Collections.reverseOrder());
+         if(values.size() != 0)
+         {
+             max_value = values.get(0);
+            return max_value;
+         }else{
+             return -1;
+         }
+    }
 }
+
